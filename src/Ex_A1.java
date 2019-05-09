47174b82d942a20e7ef66a5d24ad166871c64ecb
Judith Lang-Ladurner <judithmarialang@gmail.com>
2019-05-09 10:08:19 +0200

    changes < to <= in loop

--- src/Ex_A1.java
+++ src/Ex_A1.java
@@ -5,7 +5,7 @@
 public class Ex_A1 {
 
     public static void main(String[] args) {
-        for(int i=0; i < 100; i++) {
+        for(int i=0; i <= 100; i++) {
 
             System.out.println("i is: " +  i);
 
