c46514471107c8b0e06504ec59778b401ff299dc
Judith Lang-Ladurner <judithmarialang@gmail.com>
2019-05-09 09:52:06 +0200

    Basic A1

---
+++ src/Ex_A1.java
@@ -0,0 +1,13 @@
+//A1: Write a program that prints the integers (whole numbers) from 1 to 100.
+// The program output should be:
+
+public class Ex_A1 {
+
+    public static void main(String[] args) {
+        for(int i=0; i < 100; i++) {
+
+            System.out.println("i is: " + i);
+
+        }
+    }
+}
