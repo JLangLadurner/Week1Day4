5fe5ae27c6edce51b1de0051c3cae4b40b91e0f8
Annina Henninger <annina.henninger@aon.at>
2019-05-09 21:52:21 +0200

    Adds our personal challenge: Palindrome-check

--- src/Palindrom_check.java
+++ src/Palindrom_check.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 
+/**
+ * Programm zum Testen ob ein Wort ein Palindrom ist
+ */
+
 public class Palindrom_check {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter to check if it's a palindrom");
-
+        System.out.println("Enter to check if it's a palindrom");   //Draculasalucard, Retsinakanister, Kajak, Reittier,
+                                                                    //Rentner,
         String word = sc.nextLine();
-
         boolean isPalindrome = false;
 
         char[] letter = word.toCharArray();
 
-        for(int i=0; i<letter.length; i++){
-            for(int j=letter.length-1; j>0; j--){
-                if(letter[i] == letter[j]){
-                    isPalindrome = true;
-                }
+        int max = letter.length-1;
+        for(int i=0; i<=max; i++){
+           if(letter[i] == letter[max-i]){              //compares letters upwards and from the other side downwards
+                isPalindrome = true;
             }
         }
         if(isPalindrome){
